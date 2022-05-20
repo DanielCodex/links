832171be3d98735755d9887f64a7ef872f88474c
DanielCodex <danialups1997@gmail.com>
2022-05-20 21:07:49 +0200

    add max-width, it's easier for me

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        "3xl": "rgba(149, 157, 165, 0.2) 0px 8px 24px;"
-      }
+        "3xl": "rgba(149, 157, 165, 0.2) 0px 8px 24px;",
+      },
     },
     screens: {
-      "sm": {"max": "649px"}
-    }
+      md: { max: "767px" },
+    },
   },
   plugins: [],
 };
