ac486d9949157de43598517acc562045ea8eb25a
DanielCodex <danialups1997@gmail.com>
2022-05-20 21:08:13 +0200

    have trouble with responsive design
    i had to thought about responsive design from the first i started
    the project

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,7 @@
 // for centering the entire element in the middle
 function Container({ children }) {
   return (
-    <div className="flex bg-white flex-col h-screen w-screen items-center justify-center dark:bg-[#16181d] sm:mt-16">
+    <div className="flex bg-white flex-col h-screen w-screen items-center justify-center dark:bg-[#16181d] md:mt-16">
       {children}
     </div>
   );
